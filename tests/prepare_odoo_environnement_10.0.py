15f1580fc466f74777c0ce756032443b01452917
Dirane TAFEN <dirane-externe.tafen@enedis.fr>
2018-04-11 12:37:57 +0000

    add docker-compose version for travis build

--- tests/prepare_odoo_environnement_10.0.py
+++ tests/prepare_odoo_environnement_10.0.py
@@ -1,8 +1,8 @@
 import sys
 import odoorpc
 
-odoo = odoorpc.ODOO('localhost', port=80)
-timeout_backup = odoo.config['timeoutt']
+odoo = odoorpc.ODOO('127.0.0.1', port=80)
+timeout_backup = odoo.config['timeout']
 odoo.config['timeout'] = 240
-odoo.db.create('admin', 'test_db' , false, 'fr_FR', 'password')
+odoo.db.create('admin', 'test_db' , 'False', 'fr_FR', 'password')
 odoo.config['timeout'] = timeout_backup
